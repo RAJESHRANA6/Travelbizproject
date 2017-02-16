20d0c1850b4926db034307f12c7a5bb74d037547
RAJESHRANA6 <rajeshheaven6@gmail.com>
2017-02-16 11:46:56 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,28 @@
+<?php
+$title= "Home";
+$content= '<img src="images/travelbiz.png" class="imgleft" /> 
+<h3> Bus Reservation system </h3>
+<p>
+    Bus Reservation System is designed to automate the online ticket purchasing 
+    through an easy-to-use online bus booking system. Embed our online bus ticketing system 
+    into our website and enable our customers to book tickets for various routes 
+    and destinations. With the bus ticket reservation system, you can manage 
+    reservations, client data, and passenger lists. You can also schedule routes, set seat 
+    availability, upload an interactive seat map and let customers select their seats.
+</p>
+<img src="images/card.jpg" class="imgRight" />
+<h3> Easy way to plan your trip </h3>
+<p>
+    Help our customers organize their trips and daily commutes more easily. Process bookings and payments online, manage your business more efficiently. Check the key features that come standard with the bus ticket reservation system. 
+    To request any other fine-tuning, just contact us!
+</p>
+<img src="images/delhi.jpg" class="imgleft" />
+<h3> My travelbiz </h3>
+<p>
+    The easiest bus booking software to create routes with start, end and stop destinations, 
+    manage time traveling and bus schedules.
+</p>';
+
+include 'template.php';
+
+?>
---
+++ template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?> </title>
+        <link rel="stylesheet" type="text/css" href="styles/stylesheet.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">
+        </div>
+            
+            <nav id="navigation">
+                <ul id="nav">
+               <li><a href="index.php">Home</a></li>     
+                <li><a href="#">About</a></li>
+                <li><a href="#">Bus info</a></li>
+                <li><a href="#">Help</a></li>
+                </ul>  
+        </nav>
+            <div id="content_area">
+              <?php echo $content; ?>  
+            </div>
+            
+            <div id="sidebar">
+           
+            </div>
+            <footer>
+                <p> all rights reserved </p>
+            </footer>
+        </div>
+          
+    </body>
+</html>

62ca28e00aeebe264d76191450883fe86e832d70
RAJESHRANA6 <rajeshheaven6@gmail.com>
2017-02-18 05:48:21 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,121 @@
+<html>
+    
+    <head>
+        
+        <title>login</title>
+    <body>
+        <table border="2" cellpadding="10"> 
+            <tr>
+                <td align="center"> BUS NAME </td>
+                <td align="center"> ROUTE </td>
+                <td align="center"> BUS_NO </td>
+                <td align="center"> NO OF SEATS</td>
+                <td align="center"> OPERATIONAL DAY </td>
+            
+            </tr>
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> delhi to Mathura </td>
+                <td align="center"> 572 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to sun </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> AB TRAVEL </td>
+                <td align="center"> delhi to Kanpur </td>
+                <td align="center"> 647 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to wed </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> R K TRAVEL </td>
+                <td align="center"> delhi to agra </td>
+                <td align="center"> 372 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> VOLVO </td>
+                <td align="center"> delhi to Shimla </td>
+                <td align="center"> 570 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> VOLVO </td>
+                <td align="center"> noida to chandigarh </td>
+                <td align="center"> 272 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> delhi to agra </td>
+                <td align="center"> 372 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> GURUGRAM to agra </td>
+                <td align="center"> 172 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> HARYANA to agra </td>
+                <td align="center"> 472 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> R K TRAVEL </td>
+                <td align="center"> Delhi to jammu </td>
+                <td align="center"> 472 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> Noida to Hapur </td>
+                <td align="center"> 472 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> delhi to agra </td>
+                <td align="center"> 472 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> noida to kanpur </td>
+                <td align="center"> 472 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            <tr>
+                <td align="center"> DURGA TRAVEL </td>
+                <td align="center"> noida to agra </td>
+                <td align="center"> 172 </td>
+                <td align="center"> 45 </td>
+                <td align="center"> mon to thu </td>
+            </tr>
+            
+    </head>
+</html>
+
